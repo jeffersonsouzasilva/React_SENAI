3e399b01ff71a0493297d113dbbff386e20d2611
jeffersonsouzasilva <jefferson.souzasilva777@gmail.com>
2023-08-07 21:59:33 -0300

    menu hamburguer

--- vsconect/src/pages/Home/index.tsx
+++ vsconect/src/pages/Home/index.tsx
@@ -12,6 +12,32 @@ import Linkedin from "../../assets/img/linkedin.svg"
 function Home() {
     // Paranaue de typescript e depois o return de html em baixo
 
+    function mostrarMenu(){
+        //let menu: HTMLElement | null  = document.getElementById("menu_links"); // outra opcao de tipagem
+        let menu = document.getElementById("menu_links") as HTMLCanvasElement; // Obtém o elemento do menu pelo ID //as HTMLCanvasElement identifica como html // outra opcao de tipagem
+        let sombra: any = document.getElementById("sombra"); // Obtém o elemento da sombra pelo ID
+        let menu_barras: any = document.getElementById("menu_barras"); // Obtém o elemento do botão do menu pelo ID
+
+        let body: HTMLElement = document.getElementsByTagName("body")[0]; // Obtém o elemento body (primeiro elemento)
+
+
+        if (window.getComputedStyle(menu).left != "10px") { // Verifica se a propriedade 'left' do estilo computado do elemento do menu é diferente de 10px
+            menu.style.left = "10px"; // Define a posição 'left' do menu como 10px movendo ele para direita
+            sombra.style.right = "-10vw"; // Move a sombra para a direita
+            menu_barras.setAttribute("aria-expanded", "true"); // Atualiza o atributo 'aria-expanded' para 'true'
+            menu_barras.setAttribute("aria-label", "fechar menu"); // Atualiza o atributo 'aria-label' para 'fechar menu'
+            body.style.overflow = "hidden"; // Define o overflow do body como "hidden" para evitar a rolagem da página
+        } else {
+            menu.style.left = "-300px"; // Esconde o menu deslocando-o para esquerda
+            sombra.style.right = "110vw"; // Move a sombra para a esquerda para ocultá-la
+            menu_barras.setAttribute("aria-expanded", "false"); // Atualiza o atributo 'aria-expanded' para 'false'
+            menu_barras.setAttribute("aria-label", "abrir menu"); // Atualiza o atributo 'aria-label' para 'abrir menu'
+            body.style.overflow = "auto"; // Restaura o overflow do body para o valor padrão (auto)
+        }
+        menu_barras.classList.toggle("ativo"); // Alterna a classe 'ativo' no botão do menu
+
+    }
+
     //OPCAO MANUAL
     // return (
     //     <>
@@ -171,7 +197,7 @@ function Home() {
                             aria-expanded="false"
                             aria-controls="menu_links"
                             aria-haspopup="true"
-                            // onClick="mostrarMenu()"
+                            onClick={ mostrarMenu } //em reat pode fazer uma chamada em eventos sem precisar acrescentar parenteses
                             href="#"
                         >
                             <div className="barras" />
