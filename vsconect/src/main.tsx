fe7255fa03e303028013cdd3c1b71a9bc86ee536
jeffersonsouzasilva <jefferson.souzasilva777@gmail.com>
2023-08-08 21:35:18 -0300

    criacao da pagina de servicos

--- vsconect/src/main.tsx
+++ vsconect/src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 // import App from './App'
 import './index.css'
 import Home from './pages/Home'
+import ListaServicos from './pages/ListaServicos'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode> 
-    <Home />
+    {/* <Home /> */}
+    <ListaServicos/>
   </React.StrictMode>,
 )
