48970763e17ed61b6bf683325dfff8072126a096
jeffersonsouzasilva <jefferson.souzasilva777@gmail.com>
2023-08-10 21:54:14 -0300

    atualizacao

--- vsconect/src/main.tsx
+++ vsconect/src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+
 // import App from './App'
 import './index.css'
 import Home from './pages/Home'
@@ -8,14 +10,30 @@ import ListaServicos from './pages/ListaServicos'
 import Footer from './components/Footer'
 
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode> 
-    <Header/> 
-    <Home />
-    {/* <ListaServicos/> */}
-    <Footer/>
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode> 
+//     <Header/> 
+//     {/* <Home /> */}
+//     <ListaServicos/>
+//     <Footer/>
     
-  </React.StrictMode>,
-)
+//   </React.StrictMode>
+// )
 
 // quando der erro temos sugestoes para corrigir com ctrl + .
+
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='ListaServicos' element={<ListaServicos/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  </React.StrictMode>
+)
